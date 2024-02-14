5730ce8e9b0a5594d0392a7c25a64fa8b55b79ed
Francisco García <frank_garcia_dev@outlook.com>
2024-02-13 20:50:45 -0600

    feat(app): CAR-01 Carousel done

--- src/app/(home)/_components/carousel.tsx
+++ src/app/(home)/_components/carousel.tsx
@@ -1,14 +1,67 @@
+"use client";
 import { ImageCard } from "@/app/(home)/_components/card";
 import { images } from "@/app/(home)/_components/images";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const Carousel = () => {
+  const [posI, setPosI] = useState([0, 1, 2, 3, 4]);
+
+  const handleNext = () => {
+    setPosI((prev) => {
+      const updatedI = prev.map((prevI) => (prevI + 1) % 5);
+      return updatedI;
+    });
+  };
+
+  const handlePrev = () => {
+    setPosI((prev) => {
+      const updatedI = prev.map((prevI) => (prevI + 4) % 5);
+      return updatedI;
+    });
+  };
+
   const items = images;
 
+  const positions = ["center", "left1", "left", "right", "right1"];
+
+  const cardVariants = {
+    center: { x: "0%", scale: 1, zIndex: 5 },
+    left1: { x: "-50%", scale: 0.7, zIndex: 3 },
+    left: { x: "-90%", scale: 0.5, zIndex: 2 },
+    right: { x: "90%", scale: 0.5, zIndex: 1 },
+    right1: { x: "50%", scale: 0.7, zIndex: 3 },
+  };
+
   return (
-    <div className="flex justify-between gap-4">
-      {items.map((item) => {
-        return <ImageCard key={`card-image-${item.name}`} {...item} />;
-      })}
+    <div className="flex items-center flex-col justify-center ">
+      {items.map((item, index) => (
+        <motion.div
+          key={index}
+          initial="center"
+          animate={positions[posI[index]]}
+          variants={cardVariants}
+          transition={{ duration: 0.5 }}
+          style={{ width: "20%", position: "absolute" }}
+        >
+          <ImageCard {...item} />
+        </motion.div>
+      ))}
+      <div className="flex pt-10 flex-row gap-3">
+        <Button
+          className="text-white mt-[30rem] bg-zinc-500 transition-colors  py-2 px-4"
+          onClick={handlePrev}
+        >
+          Anterior
+        </Button>
+        <Button
+          className="text-white mt-[30rem]  bg-zinc-500 transition-colors py-2 px-4"
+          onClick={handleNext}
+        >
+          Siguiente
+        </Button>
+      </div>
     </div>
   );
 };
--- src/app/(home)/_components/images.ts
+++ src/app/(home)/_components/images.ts
@@ -6,27 +6,27 @@ export interface ImageItem {
 
 export const images: ImageItem[] = [
   {
-    src: "/product-example.png",
+    src: "/test-1.jpg",
     name: "image 1",
     description: "This is the first image",
   },
   {
-    src: "/product-example.png",
+    src: "/test-2.jpg",
     name: "image 2",
     description: "This is the second image",
   },
   {
-    src: "/product-example.png",
+    src: "/test-3.jpg",
     name: "image 3",
     description: "This is the third image",
   },
   {
-    src: "/product-example.png",
+    src: "/test-4.jpg",
     name: "image 4",
     description: "This is the fourth image",
   },
   {
-    src: "/product-example.png",
+    src: "/test-5.jpg",
     name: "image 5",
     description: "This is the fifth image",
   },
